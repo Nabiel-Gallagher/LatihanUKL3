65f1d0d0c15b841153530f6203b8bfe08a58f6ac
Nabiel Gallagher <deniza_fatih_34rpl@student.smktelkom-mlg.sch.id>
2025-11-03 09:13:18 +0700

    Add files via upload

---
+++ SedangSoal1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SedangSoal1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Bilangan Bulat Positif : ");
+        int x = input.nextInt();
+
+        if (x < 0) {
+            System.out.println("Angka Harus Positif");
+        } else {
+            long faktorial = 1;
+
+            for (int i = 1; i <= x; i++) {
+                faktorial *= i;
+            }   
+            System.out.println("Faktorial dari " + x + " adalah " + faktorial);
+        }
+    }
+}
